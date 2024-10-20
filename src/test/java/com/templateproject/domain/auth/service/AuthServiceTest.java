7f9afe91ecc3339646dea74cf776b9cb1c04b218
Nameless1004 <jjho1029@gmail.com>
2024-10-20 11:56:47 +0900

    z

---
+++ src/test/java/com/templateproject/domain/auth/service/AuthServiceTest.java
@@ -0,0 +1,17 @@
+package com.templateproject.domain.auth.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AuthServiceTest {
+
+    @Test
+    public void test() throws Exception {
+        // given
+        
+        // when
+
+        // then
+    }
+}
