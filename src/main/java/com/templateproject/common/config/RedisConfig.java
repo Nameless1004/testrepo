1a5c9b50bf4326e7e0f528e5d3ab4c7acf7f14a1
Nameless1004 <jjho1029@gmail.com>
2024-10-20 11:10:29 +0900

    z

--- src/main/java/com/templateproject/common/config/RedisConfig.java
+++ src/main/java/com/templateproject/common/config/RedisConfig.java
@@ -9,6 +9,7 @@ import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -16,6 +17,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@Profile("dev")
 @Configuration
 public class RedisConfig {
 
